576f979fb66a4dcc1c7dc64d263e415c0a3e00ff
Akashjanani <akash.janani@outlook.com>
2018-05-03 14:36:19 -0500

    update(trace-client) - Additional API requests to server

--- trace-client/src/api.js
+++ trace-client/src/api.js
@@ -1,23 +1,31 @@
 import axios from 'axios';
 
 export default {
-    user: {
+    auth: {
         login: credentials =>
-            axios.post('http://localhost:3000/api/authenticate', { credentials }).then(res => res.data.user),
+            axios.post('/authenticate', { credentials }).then(res => res.data.user),
 
         signup: user =>
-            axios.post('http://localhost:3000/api/signup', { user }).then(res => res.data.user),
+            axios.post('/signup', { user }).then(res => res.data.user),
 
         confirm: token =>
-            axios.post('http://localhost:3000/api/confirmation', { token }).then(res => res.data.user),
+            axios.post('/confirmation', { token }).then(res => res.data.user),
 
         resetPasswordRequest: email =>
-            axios.post('http://localhost:3000/api/reset_password_request', { email }),
+            axios.post('/reset_password_request', { email }),
 
         validateToken: token =>
-            axios.post('http://localhost:3000/api/validate_token', { token }),
+            axios.post('/validate_token', { token }),
 
         resetPassword: data =>
-            axios.post('http://localhost:3000/api/reset_password', { data })
+            axios.post('/reset_password', { data })
+    },
+
+    sales: {
+        salesList: () =>
+            axios.get('/sales_list').then(res => res.data),
+
+        postSales: data =>
+            axios.post('/post_sales', { data })
     }
 };
