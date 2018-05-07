b65e705c42e050184fa7017e4493f008bbc440c2
Akashjanani <akash.janani@outlook.com>
2018-05-07 00:04:31 -0500

    Revert "feat(trace-server) - Added get, post and edit submission routes"
    This reverts commit 36772aef74a88fe11f1f6439f4d66fbf761acfff.

--- trace-server/app/models/users.model.js
+++ trace-server/app/models/users.model.js
@@ -1,52 +1,50 @@
-const mongoose = require("mongoose");
-var bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-import uniqueValidator from "mongoose-unique-validator";
+const mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+import uniqueValidator from 'mongoose-unique-validator';
 
 // User Schema
-const UserSchema = mongoose.Schema(
-  {
-    firstName: {
-      type: String,
-      required: true
-    },
-
-    lastName: {
-      type: String,
-      required: true
-    },
-
-    email: {
-      type: String,
-      required: true,
-      index: true,
-      lowercase: true,
-      unique: true
-    },
-
-    passwordHash: {
-      type: String,
-      required: true
-    },
-
-    userAccessRole: {
-      type: String
-    },
-
-    confirmed: {
-      type: Boolean,
-      default: false
-    },
-
-    confirmationToken: {
-      type: String,
-      default: ""
-    }
+const UserSchema = mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
   },
-  {
-    timestamps: true
+
+  lastName: {
+    type: String,
+    required: true
+  },
+
+  email: {
+    type: String,
+    required: true,
+    index: true,
+    lowercase: true,
+    unique: true
+  },
+
+  passwordHash: {
+    type: String,
+    required: true
+  }, 
+
+  userAccessRole: {
+    type: String
+  },
+
+  confirmed: {
+    type: Boolean,
+    default: false
+  },
+
+  confirmationToken: { 
+    type: String,
+    default: ''
   }
-);
+}, 
+{
+  timestamps: true
+});
 
 /**
  * Compare the passed password with the value in the database. A model method.
@@ -54,39 +52,38 @@ const UserSchema = mongoose.Schema(
  * @param {string} password
  * @returns {object} callback
  */
-UserSchema.methods.comparePassword = function comparePassword(password) {
+ UserSchema.methods.comparePassword = function comparePassword(password) {
   return bcrypt.compareSync(password, this.passwordHash);
 };
 
 UserSchema.methods.generateJWT = function generateJWT() {
   return jwt.sign(
-    {
-      id: this.id,
-      email: this.email,
-      userAccessRole: this.userAccessRole,
-      confirmed: this.confirmed
-    },
-    process.env.JWT_SECRET /*,
-    { expiresIn: 600 }*/
+  {
+    _id: this.id,
+    email: this.email,
+    userAccessRole: this.userAccessRole,
+    confirmed: this.confirmed
+  }, 
+  process.env.JWT_SECRET, { expiresIn: 600}
   );
 };
 
 UserSchema.methods.generateResetPasswordToken = function generateResetPasswordToken() {
   return jwt.sign(
-    {
-      id: this.id
-    },
-    process.env.JWT_SECRET,
-    { expiresIn: "1h" }
-  );
+  {
+    _id: this._id,
+  }, 
+  process.env.JWT_SECRET, { expiresIn: '1h' });
 };
 
 UserSchema.methods.toAuthJSON = function toAuthJSON() {
   return {
     email: this.email,
+    userAccessRole: this.userAccessRole,
+    confirmed: this.confirmed,
     token: this.generateJWT()
-  };
-};
+  }
+}
 
 //Hashing a password before saving it to the database
 UserSchema.methods.setPassword = function setPassword(password) {
@@ -99,16 +96,13 @@ UserSchema.methods.setConfirmationToken = function setConfirmationToken() {
 
 UserSchema.methods.generateConfirmationUrl = function generateConfirmationUrl() {
   return `${process.env.HOST}/confirmation/${this.confirmationToken}`;
-};
+}
 
-UserSchema.plugin(uniqueValidator, {
-  message: "This email is already in use!"
-});
+UserSchema.plugin(uniqueValidator, {message: "This email is already in use!"});
 
 UserSchema.methods.generateResetPasswordLink = function generateResetPasswordLink() {
-  return `${
-    process.env.HOST
-  }/reset_password/${this.generateResetPasswordToken()}`;
-};
+  return `${process.env.HOST}/reset_password/${this.generateResetPasswordToken()}`;
+}
 
-module.exports = mongoose.model("users", UserSchema);
+
+module.exports = mongoose.model('users', UserSchema);
