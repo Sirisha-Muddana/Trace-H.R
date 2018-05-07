3ff5f8dfffc3aedb480030ed0cc99e35d3a7a52a
Akashjanani <akash.janani@outlook.com>
2018-05-07 00:13:12 -0500

    feat(trace-server) - Add get, post and update routes for submissions.

--- trace-server/app/models/sales.model.js
+++ trace-server/app/models/sales.model.js
@@ -1,70 +1,70 @@
-const mongoose = require('mongoose');
-var bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-import uniqueValidator from 'mongoose-unique-validator';
-const moment = require('moment')
+const mongoose = require("mongoose");
+var bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+import uniqueValidator from "mongoose-unique-validator";
+const moment = require("moment");
 
 // User Schema
-const SalesSchema = mongoose.Schema({
+const SalesSchema = mongoose.Schema(
+  {
+    consultantName: {
+      type: String,
+      required: true
+    },
 
-  consultantName: {
-    type: String,
-    required: true                
-  },
+    skillset: {
+      type: String,
+      required: true
+    },
 
-  skillset: {
-    type: String,
-    required: true
-  },
+    location: {
+      type: String,
+      required: true
+    },
 
-  location: {
-    type: String,
-    required: true
-  },
+    billingRate: {
+      type: Number,
+      required: true
+    },
 
-  billingRate: {
-    type: Number,
-    required: true
-  }, 
+    vendor: {
+      type: String,
+      required: true
+    },
 
-  vendor: {
-    type: String,
-    required: true
-  },
+    client: {
+      type: String,
+      required: true
+    },
 
-  client: {
-    type: String,
-    required: true
-  },
+    interviewDate: {
+      type: String,
+      default: moment().format("YYYY-MM-DD")
+    },
 
-  interviewDate: { 
-    type: String,
-    default: moment().format('YYYY-MM-DD')
-  },
+    interviewType: {
+      type: String,
+      required: true
+    },
 
-  interviewType: {
-    type: String,
-    required: true
-  },
+    feedback: {
+      type: String,
+      required: false
+    },
 
-  feedback: {
-    type: String,
-    required: false
-  },
+    recruiter: {
+      type: String,
+      required: false
+    },
 
-  recruiter: {
-    type: String,
-    required: false
-  },
-
-  createdAt: {
-    type: String,
-    default: moment(Date.now()).format('YYYY-MM-DD')
+    createdAt: {
+      type: String,
+      default: moment(Date.now()).format("YYYY-MM-DD")
+    }
   }
-} 
-/*{
+  /*{
   timestamps: true
-}*/);
+}*/
+);
 
-
-module.exports = mongoose.model('sales', SalesSchema);
+module.exports = mongoose.model("sales", SalesSchema);
--- trace-server/app/routes/users.routes.js
+++ trace-server/app/routes/users.routes.js
@@ -1,37 +1,48 @@
-const passport = require('passport');
+const passport = require("passport");
 
-module.exports = (app) => {
-    const auth = require('../controllers/auth.controller.js');
-    const sales = require('../controllers/sales.controller.js');
+module.exports = app => {
+  const auth = require("../controllers/auth.controller.js");
+  const sales = require("../controllers/sales.controller.js");
 
-    // AUTH ROUTES
-    app.post('/api/signup', auth.create);
+  // AUTH ROUTES
+  app.post("/api/signup", auth.create);
 
-    app.post('/api/authenticate', auth.authenticate);
+  app.post("/api/authenticate", auth.authenticate);
 
-    app.post('/api/confirmation', auth.confirmation);
+  app.post("/api/confirmation", auth.confirmation);
 
-    app.post('/api/reset_password_request', auth.resetPasswordRequest);
+  app.post("/api/reset_password_request", auth.resetPasswordRequest);
 
-    app.post('/api/validate_token', auth.validateToken);
+  app.post("/api/validate_token", auth.validateToken);
 
-    app.post('/api/reset_password', auth.resetPassword);
+  app.post("/api/reset_password", auth.resetPassword);
 
+  //USER ROUTES
+  // Retrieve all users
+  app.get(
+    "/api/users",
+    passport.authenticate("jwt", { session: false }),
+    sales.findAll
+  );
 
-    //USER ROUTES
-    // Retrieve all users
-    app.get('/api/users', passport.authenticate('jwt', { session: false }), sales.findAll);
+  // Retrieve a single user with userId
+  app.get("/api/users/:userId", sales.findOne);
 
-    // Retrieve a single user with userId
-    app.get('/api/users/:userId', sales.findOne);
+  // Update a user with userId
+  app.put("/api/users/:userId", sales.update);
 
-    // Update a user with userId
-    app.put('/api/users/:userId', sales.update);
+  // Delete a user with userId
+  app.delete("/api/users/:userId", sales.delete);
 
-    // Delete a user with userId
-    app.delete('/api/users/:userId', sales.delete);
+  app.get(
+    "/api/sales_list",
+    passport.authenticate("jwt", { session: false }),
+    sales.salesList
+  );
 
-    app.get('/api/sales_list', passport.authenticate('jwt', {session: false}), sales.salesList);
-
-    app.post('/api/post_sales', passport.authenticate('jwt', {session: false}),sales.postSales);
-}
+  app.post(
+    "/api/post_sales",
+    passport.authenticate("jwt", { session: false }),
+    sales.postSales
+  );
+};
