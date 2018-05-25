99be809de56b65672505b04ac3e376e99ce2411c
Akashjanani <akash.janani@outlook.com>
2018-05-25 13:13:39 -0500

    update(trace-client) - Fixed signup bug

--- trace-client/src/api.js
+++ trace-client/src/api.js
@@ -5,23 +5,22 @@ export default {
     login: credentials =>
       axios.post("/authenticate", credentials).then(res => res.data.user),
 
-    signup: user => axios.post("/signup", { user }).then(res => res.data.user),
+    signup: user => axios.post("/register", user).then(res => res.data.user),
 
     confirm: token =>
-      axios.post("/confirmation", { token }).then(res => res.data.user),
+      axios.post("/confirmation", token).then(res => res.data.user),
 
-    resetPasswordRequest: email =>
-      axios.post("/reset_password_request", { email }),
+    resetPasswordRequest: email => axios.post("/reset_password_request", email),
 
-    validateToken: token => axios.post("/validate_token", { token }),
+    validateToken: token => axios.post("/validate_token", token),
 
-    resetPassword: data => axios.post("/reset_password", { data })
+    resetPassword: data => axios.post("/reset_password", data)
   },
 
   submissions: {
     submissionList: () => axios.get("/submissions_list").then(res => res.data),
 
-    postSubmission: data => axios.post("/post_submission", { data }),
+    postSubmission: data => axios.post("/post_submission", data),
 
     getSubmission: id =>
       axios.get(`/get_submission/${id}`).then(res => res.data),
