b94b448e890aa0a3497dcb13f483a8e116b2982c
Akashjanani <akash.janani@outlook.com>
2018-05-29 17:00:10 -0500

    Implemented search functionality

--- trace-client/src/Components/Management/All Submissions/GetSubmissionsPage.js
+++ trace-client/src/Components/Management/All Submissions/GetSubmissionsPage.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import GetSubmissionsForm from "./GetSubmissionsForm";
 import { connect } from "react-redux";
 import { getSubmissions } from "../../../actions/submissionListActions";
-import { Menu, Input, Loader } from "semantic-ui-react";
+import { Loader } from "semantic-ui-react";
 
 class GetSubmissionsPage extends Component {
   componentDidMount() {
@@ -20,18 +20,15 @@ class GetSubmissionsPage extends Component {
       if (Object.keys(getSubmissions).length > 0) {
         allSubmissions = <GetSubmissionsForm getSubmissions={getSubmissions} />;
       } else {
-        allSubmissions = <h4> No submissions found for this recruiter</h4>;
+        allSubmissions = (
+          <h4 className="ml-5"> No submissions found for this recruiter</h4>
+        );
       }
     }
 
     return (
       <div>
         <h3 className="display-4 text-center">Submission List</h3>
-        <Menu secondary>
-          <Menu.Item position="right">
-            <Input className="icon" icon="search" placeholder="Search..." />
-          </Menu.Item>
-        </Menu>
         {allSubmissions}
       </div>
     );
