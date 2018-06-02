cf1705ced2a0d27e9007796210de36cae333ea88
Akashjanani <akash.janani@outlook.com>
2018-06-01 20:27:23 -0500

    Added search functionality

--- trace-client/src/Components/Users/UsersPage.js
+++ trace-client/src/Components/Users/UsersPage.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import UsersInfo from "./UsersInfo";
 import { connect } from "react-redux";
 import { fetchUsers } from "../../actions/userActions";
-import { Menu, Input, Loader } from "semantic-ui-react";
+import { Loader } from "semantic-ui-react";
 
 class UsersPage extends Component {
   componentDidMount() {
@@ -26,11 +26,6 @@ class UsersPage extends Component {
     return (
       <div>
         <h1 className="display-4 text-center">Consultant List</h1>
-        <Menu secondary>
-          <Menu.Item>
-            <Input className="icon" icon="search" placeholder="Search..." />
-          </Menu.Item>
-        </Menu>
         {allUsers}
       </div>
     );
