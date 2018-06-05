6cd59ce25f608bbe7dc806e27361104e9ff83ae4
Akashjanani <akash.janani@outlook.com>
2018-06-05 11:55:29 -0500

    Fixed a bug for forgot password component where the email address entered was undefined

--- trace-client/src/actions/authActions.js
+++ trace-client/src/actions/authActions.js
@@ -28,7 +28,7 @@ export const confirm = token => dispatch =>
     dispatch(userLoggedIn(user));
   });
 
-export const resetPasswordRequest = ({ email }) => () =>
+export const resetPasswordRequest = email => () =>
   api.auth.resetPasswordRequest(email);
 
 export const validateToken = token => () => api.auth.validateToken(token);
