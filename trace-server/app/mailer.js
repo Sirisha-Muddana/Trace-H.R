312ba1ff1cb17aea807fe6946773309608250e60
Akashjanani <akash.janani@outlook.com>
2018-06-06 12:03:16 -0500

    Added kairos domain credentials for sending emails via nodemailer

--- trace-server/app/mailer.js
+++ trace-server/app/mailer.js
@@ -1,47 +1,51 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
-const from = '"Trace" <info@kairostech.com';
+const from = '"Trace H.R" <trace-hr@kairostech.com>';
 
 function setup() {
-	return nodemailer.createTransport({
-		host: "smtp.mailtrap.io",
-		port: 2525,
-		auth: {
-			user: "b6321380a02508",
-			pass: "eeb0140701d66f"
-		}
-	})
+  return nodemailer.createTransport({
+    host: process.env.EMAIL_HOST, // Office 365 server
+    port: process.env.EMAIL_PORT, // secure SMTP
+    secure: false, // false for TLS - as a boolean not string - but the default is false so just remove this completely
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    },
+    tls: {
+      ciphers: "SSLv3"
+    }
+  });
 }
 
 export function sendConfirmationEmail(user) {
-	const transport = setup();
-	const email = {
-		from,
-		to: user.email,
-		subject: "Welcome to Trace",
-		text: `
+  const transport = setup();
+  const email = {
+    from,
+    to: user.email,
+    subject: "Welcome to Trace",
+    text: `
 		Welcome to Trace H.R. 
 		Please, confirm your email.
 
 		${user.generateConfirmationUrl()}
 		`
-	}
+  };
 
-	transport.sendMail(email);
+  transport.sendMail(email);
 }
 
 export function sendResetPasswordEmail(user) {
-	const transport = setup();
-	const email = {
-		from,
-		to: user.email,
-		subject: "Reset Password",
-		text: `
+  const transport = setup();
+  const email = {
+    from,
+    to: user.email,
+    subject: "Reset Password",
+    text: `
 		To reset password follow this link:
 
 		${user.generateResetPasswordLink()}
 		`
-	}
+  };
 
-	transport.sendMail(email);
+  transport.sendMail(email);
 }
